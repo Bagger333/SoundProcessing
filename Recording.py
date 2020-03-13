471fe8e417fe77001fa700c9d3cfd9537c1c6d4e
Bagger333 <47859457+Bagger333@users.noreply.github.com>
2020-03-13 10:57:31 +0100

    recording and playing
    includes a combfilter with a working delay effect, the effect just doesnt take effect -.-

--- Recording.py
+++ Recording.py
@@ -1,12 +1,14 @@
 import pyaudio
 import numpy as np
 import sys
-
+import matplotlib.pyplot as plt
 
 CHUNK = 1024
 WIDTH = 2
 CHANNELS = 2
-RATE = 48000 #hz
+RATE = 44100 #hz
+delay = np.int(np.round(0.05*RATE))
+filterCoef = 0.5
 
 p = pyaudio.PyAudio()
 
@@ -18,12 +20,27 @@ streamIn = p.open(format=p.get_format_from_width(WIDTH),
                   frames_per_buffer=CHUNK)
 
 
+def combfiltering(inputSignal, filterCoef, delay, feedBackIsUsed=False):
+    nData = np.size(inputSignal)
+    outputSignal = np.zeros(nData)
+    for n in np.arange(nData):
+        if n < delay:
+            outputSignal[n] = inputSignal[n]
+        else:
+            if feedBackIsUsed:
+                outputSignal[n] = inputSignal[n]+filterCoef*outputSignal[n-delay]
+            else:
+                print("hello")
+                outputSignal[n] = inputSignal[n]+filterCoef*inputSignal[n-delay]
+    return outputSignal
+
+
 if __name__ == "__main__":
     print("* recording")
     for i in range(0, int(RATE / CHUNK * sys.maxsize)):
-        data = np.fromstring(streamIn.read(CHUNK), dtype=np.int16)
-        streamIn.write(data, CHUNK)
-        print(data)
+        data = np.fromstring(streamIn.read(CHUNK), dtype=float)
+        modifiedSignal = combfiltering(data, filterCoef, delay, False)
+        streamIn.write(modifiedSignal, CHUNK)
 
     print("*Stopped Recording")
 
